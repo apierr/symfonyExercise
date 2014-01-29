5cc0b9110d6681d40a0d4725a96777e4ad868cb1
Antonio Pierro <antonio.pierro@gmail.com>
2014-01-29 10:04:28 +0100

    generated the 'Bundle namespace: Tutorial\AnimalsBundle'

---
+++ src/Tutorial/AnimalsBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tutorial\AnimalsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('TutorialAnimalsBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Tutorial/AnimalsBundle/TutorialAnimalsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tutorial\AnimalsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TutorialAnimalsBundle extends Bundle
+{
+}
