ac9753947dc044c01882f2112196d78b2a016459
Antonio Pierro <antonio.pierro@gmail.com>
2014-01-29 17:13:40 +0100

    generate the type animal for the form by doctrine:generate:form TutorialAnimalsBundle:Animal

---
+++ src/Tutorial/AnimalsBundle/Form/AnimalType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tutorial\AnimalsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AnimalType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('url')
+            ->add('date')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Tutorial\AnimalsBundle\Entity\Animal'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'tutorial_animalsbundle_animal';
+    }
+}
