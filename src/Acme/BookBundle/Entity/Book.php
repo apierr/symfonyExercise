879c8149c53a1f25c5012cf398995111aba41ec8
Antonio Pierro <antonio.pierro@gmail.com>
2014-02-10 10:08:58 +0100

    first commit of Book entity by doctrine:generate:entity

---
+++ src/Acme/BookBundle/Entity/Book.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Acme\BookBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Book
+ */
+class Book
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
